340dda431b4b0899c02581ef690ffbf88147a259
yeonwoo.jo <yeonwoo.jo@nhnent.com>
2018-08-04 21:12:03 +0900

    Remove unused Link

--- website/src/components/App.js
+++ website/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { NavigationBar } from './navigationBar/NavigationBar'
 import { Home } from './Home/Home';
